b935245f295b2e04813ba09d09ff5a110f9348b5
thisissareer <74141145+thisissareer@users.noreply.github.com>
2020-11-08 19:19:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-QUERY = '#java'
-  
+# Here you can change the key word. 
+QUERY = '#python'
+# if you want to turn off like or follow change True to False  
 LIKE = True
 FOLLOW = False
+# Down below you can change the sleep time.
 SLEEP_TIME = 5
