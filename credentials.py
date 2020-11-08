1120ea8302132e8c61348f32eb1b8a89ff1990da
thisissareer <74141145+thisissareer@users.noreply.github.com>
2020-11-08 19:15:27 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 
-consumer_key = 'WTngmY2llrGJHJ3SZWeMHS9NR' 
-consumer_secret = 'HSzOY0RSpu2ijRmVWRGoN14IzUdUTv6larqz48i4nHA7MJK8pA' 
-access_token = '1320257831338037251-o9cao2XkXhIiOaFhesOeTPC1VJvsbs' 
-access_token_secret = 'P13XmV9PmSJSIpDFSdejMznd1fJk6ZvxOeIyJxSUMYiSd'
+consumer_key = '----' 
+consumer_secret = '----' 
+access_token = '----' 
+access_token_secret = '----'
